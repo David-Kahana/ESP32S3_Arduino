2d9d74ffd88c150bbb3bc27eb9e2a7786436ded2
David-Kahana <david.kahana@gmail.com>
2024-08-17 13:18:25 +0300

    more basic tests

--- basic/GetChipID/GetChipID.ino
+++ basic/GetChipID/GetChipID.ino
@@ -26,5 +26,10 @@ void loop() {
   Serial.print("Chip ID: ");
   Serial.println(chipId);
 
+  Serial.printf("Total heap: %d\n", ESP.getHeapSize());
+  Serial.printf("Free heap: %d\n", ESP.getFreeHeap());
+  Serial.printf("Total PSRAM: %d\n", ESP.getPsramSize());
+  Serial.printf("Free PSRAM: %d\n", ESP.getFreePsram());
+
   delay(3000);
 }
