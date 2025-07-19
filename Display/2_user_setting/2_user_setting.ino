0822305d3cc5cfec929c7a04f09bcbeb5c117360
David-Kahana <david.kahana@gmail.com>
2025-07-19 13:09:28 +0300

    test rect

--- Display/2_user_setting/2_user_setting.ino
+++ Display/2_user_setting/2_user_setting.ino
@@ -5,24 +5,63 @@ LGFX display;
 
 void setup(void)
 {
+  Serial.begin(115200);
   display.init();
   display.setBrightness(255);
   display.setTextSize((std::max(display.width(), display.height()) + 255) >> 8);
-  display.fillScreen(TFT_BLACK);
+  display.setRotation(0);
+  display.setColorDepth(1);
+  display.fillScreen(TFT_WHITE);
+  Serial.printf("width: %d, height: %d\n", display.width(), display.height());
+  delay(1000);
 }
 
 uint32_t count = ~0;
 void loop(void)
 {
+  // for (uint8_t rotation = 0; rotation < 8; ++rotation)
+  // {
+  //   display.fillScreen(TFT_BLACK);
+  //   display.startWrite();
+  //   display.setRotation(rotation);
+  //   display.setColorDepth(1);
+
+  //   display.setTextColor(TFT_WHITE);
+  //   display.drawString("Hello", 30, 16);
+  //   display.drawNumber(display.getRotation(), 16, 16);
+
+  //   display.endWrite();
+  //   delay(500);
+  // }
   display.fillScreen(TFT_BLACK);
-  display.startWrite();
-  display.setRotation(++count & 7);
-  display.setColorDepth(1);
-
-  display.setTextColor(TFT_WHITE);
-  display.drawString("Hello", 30, 16);
-  display.drawNumber(display.getRotation(), 16, 16);
-
-  display.endWrite();
-  delay(1000);
+  display.drawPixel(0, 0, TFT_WHITE);
+  delay(500);
+  display.drawPixel(display.width()-1, 0, TFT_WHITE);
+  delay(500);
+  display.drawPixel(0, display.height()-1, TFT_WHITE);
+  delay(500);
+  display.drawPixel(display.width()-1, display.height()-1, TFT_WHITE);
+  delay(500);
+  display.drawPixel(display.width()/2, display.height()/2, TFT_WHITE);
+  delay(500);
+  display.drawFastVLine(10, 0, 63, TFT_WHITE); 
+  delay(500);
+  display.drawFastHLine(0, 10, 127, TFT_WHITE); 
+  delay(500);
+  display.fillRect(display.width()/2-20, display.height()/2-20, 40, 40, TFT_WHITE);
+  delay(500);
+  display.fillRect(display.width()/2-20, display.height()/2-20, 40, 40, TFT_BLACK);
+  display.fillRoundRect(display.width()/2-20, display.height()/2-20, 40, 40, 5, TFT_WHITE); 
+  // for (uint16_t b = 0; b < display.height(); ++b)
+  // {
+  //   display.drawFastHLine(0, b, display.width()-1, b+1); 
+  // }
+  // delay(2000);
+  // for (uint16_t a = 0; a < display.width()/2; ++a)
+  // {
+  //   display.fillRect(a, 0, display.width()-(2*a), display.height()-1, 255);
+  //   delay(300);
+  //   display.fillScreen(TFT_BLACK);
+  // }
+  delay(2000);
 }
